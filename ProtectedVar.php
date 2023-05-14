7fcc3915a857e8b83e3cca415293107ff4faf3ac
DeathScore13 <deathscore13@gmail.com>
2023-05-15 02:54:36 +0300

    Добавление установки/возврата по ссылке

--- ProtectedVar.php
+++ ProtectedVar.php
@@ -10,29 +10,45 @@
 trait ProtectedVar
 {
     /**
-     * Установка/получение значения переменной
+     * Установка/получение значения переменной по ссылке
+     * 
+     * @param string $name      Имя переменной
+     * @param mixed &$value     Значение переменной (только при инициализации)
+     * @param bool $ref         Синхронизация значения с переменной переданной в параметр $value (только при инициализации)
+     * @param bool $clone       Возвращать значение не по ссылке (только при инициализации)
+     * 
+     * @return mixed            Значение переменной
      */
-    public static function __callStatic(string $name, array $arg): mixed
+    public static function &var(string $name, mixed &$value = 0, bool $ref = false, bool $clone = true): mixed
     {
-        static $var = [];
-        static $clone = [];
+        static $vars = [];
+        static $clones = [];
 
-        if (!isset($var[$name]))
+        if (!isset($vars[$name]))
         {
-            if (!isset($arg[0]))
+            if (func_num_args() === 1)
                 throw new Exception('Undefined variable $'.$name);
             
-            $var[$name] = $arg[0];
+            if ($ref)
+                $vars[$name] = &$value;
+            else
+                $vars[$name] = is_object($value) ? (clone $value) : $value;
 
-            if (isset($arg[1]) && $arg[1] === true)
-                $clone[] = $name;
+            if ($clone)
+                $clones[] = $name;
         }
         
-        return in_array($name, $clone) ? (clone $var[$name]) : $var[$name];
+        if (in_array($name, $clones))
+        {
+            $buffer = is_object($vars[$name]) ? (clone $vars[$name]) : $vars[$name];
+            return $buffer;
+        }
+        
+        return $vars[$name];
     }
     
     /**
-     * Проверяет существование переменной
+     * Проверка существования переменной
      * 
      * @param string $name      Имя переменной
      * 
@@ -42,12 +58,23 @@ trait ProtectedVar
     {
         try
         {
-            self::class::$name();
+            self::class::var($name);
         }
         catch (Exception $e)
         {
             return false;
         }
         return true;
+    }
+
+    /**
+     * Установка/получение значения переменной
+     * НЕ УМЕЕТ синхронизировать значение с переменной переданной в параметр $value, если это не объект
+     * НЕ УМЕЕТ возвращать значение по ссылке, если это не объект
+     * Зато имеет красивый синтаксис Safe::name()
+     */
+    public static function __callStatic(string $name, array $arg): mixed
+    {
+        return self::var($name, ...$arg);
     }
 }
